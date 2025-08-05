118a3cdbcd84bcaa361b59e5f4b20e7a920df166
awrmurphy <awrmurphy@gmail.com>
2025-08-05 13:05:05 -0400

    created xhr get function

--- script.js
+++ script.js
@@ -25,3 +25,22 @@ function display(data){
     dataDiv.appendChild(document.createElement("br"));
     dataDiv.appendChild(postContent);
 }
+
+document.getElementById("xhr").addEventListener('click',function(){
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET',"https://jsonplaceholder.typicode.com/posts/2",true);
+
+    xhr.onreadystatechange = function(){
+        if (xhr.readyState===4){
+            if(xhr.status===200){
+                const data = JSON.parse(xhr.responseText);
+                console.log(data);
+                display(data);
+            } else{
+                console.error('Error fetching data: ',xhr.statusText);
+            }
+        }
+        
+    };
+    xhr.send();
+});
